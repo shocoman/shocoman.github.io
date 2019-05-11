77ae8ed7327d7474600d4dfa07b6c46a6a6d6a0b
shocoman <shoco13@gmail.com>
2019-05-11 16:55:41 +0700

    1

---
+++ code.js
@@ -0,0 +1,34 @@
+$(function(){
+    
+    
+    login_request = function(){
+        
+        console.log('gdffd');
+        
+        
+        $.ajax({
+            url: 'http://193.218.136.174:8080/cabinet/rest/auth/login',
+            method: 'POST',
+            contentType: 'application/json',
+            dataType: 'application/json',
+            data: {
+                'username': 'vkulinenko-ki17',
+                'password': 'chocolates22',
+                'appToken': 'XO6dmVqroG'
+            }
+            
+            
+        }).done(function(html){
+            console.log(html);
+        })
+        
+        
+        
+        
+    }
+    
+    
+    $('#enter_button').click(login_request);
+    
+    
+});
