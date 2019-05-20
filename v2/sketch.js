161b6c38b84bd67e04a55684403961c14a953900
shocoman <shoco13@gmail.com>
2019-05-20 18:29:01 +0700

    3

--- v2/sketch.js
+++ v2/sketch.js
@@ -10,48 +10,49 @@ let frame_rate_slider;
 
 function setup() {
   frameRate(60);
-  createCanvas(600, 600);
+  createCanvas(1000, 600);
 
   life = new GameOfLife();
   life.init();
-  
-  
+
+
   hamster = new Hamster();
 
-  createElement('br'); 
+  createElement('br');
   launch_btn = createButton('Launch');
   launch_btn.mousePressed(function(e) {
     if (!hamster.is_jumper && !hamster.is_shooter && hamster.pos.y > height)
       hamster.shoot();
   });
-  
+
   paused = false;
   pause_btn = createButton('Pause');
   pause_btn.mousePressed(life.pause);
-  
+
   stop_btn = createButton('Stop and Restore');
   stop_btn.mousePressed(life.stop);
   life.stop();
 
   clear_btn = createButton('Clear');
   clear_btn.mousePressed(life.clear_field_state);
-  
-  createElement('br'); 
+
+  createElement('br');
   launch_power_slider = createSlider(0, 100, 10);
   createElement('label', ' <= Spawning coefficient (0-100%)');
-  
+
   createElement('br');
-  frame_rate_slider = createSlider(1, 20, 5); createElement('label', ' <= Speed');
+  frame_rate_slider = createSlider(1, 20, 5);
+  createElement('label', ' <= Speed');
 }
 
 
 
 function draw() {
   background(111);
-  
+
   life.draw();
 
-  if (!(frameCount % ceil(1/frame_rate_slider.value()*20)) && !paused) {
+  if (!(frameCount % ceil(1 / frame_rate_slider.value() * 20)) && !paused) {
     life.make_turn();
   }
 
@@ -76,7 +77,7 @@ class Hamster {
   constructor() {
     this.img = loadImage('https://shocoman.github.io/v2/chibi.png');
 
-    this.pos = createVector(0, 400);
+    this.pos = createVector(0, 500);
     this.vel = createVector(0, 0);
     this.gravity = createVector(0, 0.1);
 
@@ -115,8 +116,8 @@ class Hamster {
     translate(width + this.pos.x, height + this.pos.y);
     rotate(frameCount * this.rotation_speed);
 
-    let size = map(this.vel.y, -13, 5, 0.15, 5);
-    image(this.img, 0, 0, width / 4 / size, height / 4 / size);
+    let size = map(this.vel.y, -13, 5, 0.2, 5);
+    image(this.img, 0, 0, width / 4 / size * height / width, height / 4 / size);
   }
 
   shoot() {
@@ -126,10 +127,10 @@ class Hamster {
     this.pos.set(random(-width, 0), height / 2);
     let launch_power = createVector(this.pos.x + width / 2, this.pos.y + height);
     launch_power.normalize();
-    launch_power.mult(-12 * width / 600);
+    launch_power.mult(-13);
     this.vel = launch_power;
 
-    this.rotation_speed = random(0.08, 0.25) * (launch_power.x >= 0 ? 1 : -1);
+    this.rotation_speed = map(launch_power.x, -10, 10, -0.1, 0.1) * 5;
   }
 
 
@@ -198,7 +199,7 @@ class GameOfLife {
 
 
   spawn_cells() {
-    let number = this.cells_x * this.cells_y * launch_power_slider.value()/100;
+    let number = this.cells_x * this.cells_y * launch_power_slider.value() / 100;
 
     let cells = []
     while (number > 0) {
@@ -314,7 +315,7 @@ function doubleClicked() {
   let x = int(map(mouseX, 0, width, 0, life.cells_x));
   let y = int(map(mouseY, 0, height, 0, life.cells_y));
 
-  if (life.field[x][y] == 0) { 
+  if (life.field[x][y] == 0) {
     life.field[x][y] = 1;
   } else {
     life.field[x][y] = 0;
