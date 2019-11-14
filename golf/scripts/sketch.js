b44e40cf39ac4e43ee827de52c91b78adf79f2fa
Victor <shoco13@gmail.com>
2019-11-14 22:34:59 +0700

    Update sketch.js

--- golf/scripts/sketch.js
+++ golf/scripts/sketch.js
@@ -1,88 +1,100 @@
 let ball;
 
-// let lineSegment;
+let dots;
+let lineSegments;
+let finishMark;
 
-let dots = [];
-let lineSegments = [];
+let noiseOffset = 0;
+
+let landSlideSpeed = 5;
 
 function setup() {
 	createCanvas(600, 600);
 	frameRate(60);
 
-	
-	// lineSegment = new LineSegment(100,500,500,300);
-	// lineSegments.push(new LineSegment(300,300,500,400));
-	// lineSegments.push(new LineSegment(100,500,300,300));
-	// lineSegments.push(new LineSegment(100,500,500,500));
-	// lineSegments.push(new LineSegment(100,100,100,500));
+	dots = [];
+	lineSegments = [];
 
-
-	generateDots(dots);
-	ellipse(dots[0].x, dots[0].y, 40);
-	ellipse(dots[dots.length-1].x, dots[dots.length-1].y,20);
-
-	// start point
-	lineSegments.push(new LineSegment(0, dots[0].y, dots[0].x, dots[0].y));
-	// end point
-	lineSegments.push(new LineSegment(dots[dots.length-1].x, dots[dots.length-1].y, width, dots[dots.length-1].y));
-
-	dotsToLineSegments(dots);
-
-	ball = new Ball(dots[0].x/2, dots[0].y - 5);
-}
-
-function dotsToLineSegments(dots){
-	for (let i = 0; i < dots.length-1; i++){
-		lineSegments.push(new LineSegment(dots[i].x, dots[i].y, dots[i+1].x, dots[i+1].y));
-	}
-}
-
-function generateDots(dots){
-	let startPos = 50;
-	let endPos = height - 50;
-	let step = 5;
-
-	for (let i = 0; i < (width - startPos - (height - endPos)) / step; i++){
-		let noiseValue = noise(i/25);
-		noiseValue = map(noiseValue, 0, 1, height, height/3);
-		dots.push(new Dot(i * step + startPos, noiseValue));
-	}
-
+	initLevel();
 }
 
 function draw() {
-	background(220);
+	background(255, 155);
 
 	ball.update(lineSegments);
-	ball.draw();
+	finishCheck(ball);
 
-	//lineSegment.draw();
+	ball.draw();
 
 	lineSegments.forEach(l => {
 		l.draw();
 	});
+}
+function initLevel() {
+	dots = [];
+	lineSegments.forEach(l => {
+		l.move_away = true;
+	});
 
+	generateDots(dots);
 
+	ball = new Ball(dots[0].x / 2, dots[0].y - 20);
+	initStartPoint(ball, lineSegments, dots);
+
+	dotsToLineSegments(dots);
+
+	initEndPoint(ball, lineSegments, dots);
 }
 
-function vectorProject(vec1, vec2){
+function dotsToLineSegments(dots) {
+	for (let i = 0; i < dots.length - 1; i++) {
+		lineSegments.push(new LineSegment(dots[i].x, dots[i].y, dots[i + 1].x, dots[i + 1].y));
+	}
+}
+
+function generateDots(dots) {
+	let startPos = 50;
+	let endPos = height - 50;
+	let step = 5;
+	let numOfParts = (width - startPos - (height - endPos)) / step;
+	for (let i = 0; i < numOfParts; i++) {
+		let noiseValue = noise(i / 25 + noiseOffset);
+		noiseValue = map(noiseValue, 0, 1, height, height / 3);
+		dots.push(new Dot(i * step + startPos, noiseValue));
+	}
+
+	noiseOffset += numOfParts / 25;
+}
+
+function finishCheck(ball) {
+	if (finishMark.dist(ball.pos) <= ball.r && ball.vel.mag() < 1) {
+		initLevel();
+	}
+}
+
+function vectorProject(vec1, vec2) {
 	let projection = vec1.copy();
-	return projection.mult(vec1.dot(vec2) / (vec1.magSq()));
+	return projection.mult(vec1.dot(vec2) / vec1.magSq());
 }
 
 class Ball {
-	constructor(x,y){
-		this.pos = createVector(x,y);
-		this.old_pos = this.pos;
-		this.vel = createVector(0,0);
-		this.acc = createVector(0,0.1);
+	constructor(x, y) {
+		this.start_offset = width - 55;
+		this.move_away = true;
 
-		this.r = 5;
+		this.pos = createVector(x + this.start_offset, y);
+		this.old_pos = this.pos;
+		this.vel = createVector(0, 0);
+		this.acc = createVector(0, 0.1);
+
+		this.r = 6;
 		this.friction_coeff = 0.99;
 		this.jumpness_coeff = 0.8;
 	}
 
-	boundariesCheck(){
+	boundariesCheck() {
+		if (this.move_away == true) return;
+
 		if (this.pos.y + this.r > height) {
 			this.pos.y = height - this.r;
 			this.vel.y *= -1;
@@ -104,7 +116,7 @@ class Ball {
 		}
 	}
 
-	update(lineSegments){
+	update(lineSegments) {
 		this.old_pos = this.pos.copy();
 		this.vel.add(this.acc);
 		this.vel.x *= this.friction_coeff;
@@ -113,32 +125,31 @@ class Ball {
 
 		this.boundariesCheck();
 
-		//this.collisionCheck(lineSegment);
-
 		lineSegments.forEach(l => {
 			this.collisionCheck(l);
 		});
 
+		if (this.move_away && this.start_offset > 0) {
+			this.pos.x -= landSlideSpeed;
+			this.start_offset -= landSlideSpeed;
+		} else {
+			this.move_away = false;
+		}
 	}
 
-	collisionCheck(lineSegment){
+	collisionCheck(lineSegment) {
 		let vecToBall = p5.Vector.sub(this.pos, lineSegment.p1);
 		let vecToEnd = p5.Vector.sub(lineSegment.p2, lineSegment.p1);
 
 		let projectVector = vectorProject(vecToEnd, vecToBall);
-		// ellipse(lineSegment.p1.x + projectVector.x, lineSegment.p1.y + projectVector.y, 20);
-
-		let intersectPoint = createVector(lineSegment.p1.x + projectVector.x,
-											lineSegment.p1.y + projectVector.y);
+		let intersectPoint = createVector(lineSegment.p1.x + projectVector.x, lineSegment.p1.y + projectVector.y);
 
 		let projectLength = projectVector.mag() * Math.sign(projectVector.x);
-		if (projectLength >= vecToEnd.mag() + this.r || projectLength <= -this.r){
-
+		if (projectLength >= vecToEnd.mag() + this.r || projectLength <= -this.r) {
 		} else if (projectLength >= vecToEnd.mag() || projectLength <= 0) {
 			this.pointCollision(lineSegment.p1);
 			this.pointCollision(lineSegment.p2);
-		} else if (p5.Vector.dist(intersectPoint, this.pos) <= this.r){
-			
+		} else if (p5.Vector.dist(intersectPoint, this.pos) <= this.r) {
 			let perpVector = p5.Vector.sub(this.pos, intersectPoint);
 			let angle = atan2(perpVector.y, perpVector.x) - atan2(this.vel.y, this.vel.x);
 
@@ -150,61 +161,118 @@ class Ball {
 		}
 	}
 
-	pointCollision(point){
-		if (point.dist(this.pos) < this.r){
-			this.vel = p5.Vector.sub(this.pos, point).normalize().mult(this.vel.mag());
+	pointCollision(point) {
+		if (point.dist(this.pos) < this.r) {
+			this.vel = p5.Vector.sub(this.pos, point)
+				.normalize()
+				.mult(this.vel.mag());
 			this.pos = this.old_pos.copy();
 			this.vel.y *= this.jumpness_coeff;
 		}
-		
 	}
 
-	draw(){
-		ellipse(this.pos.x, this.pos.y, this.r*2);
-
-		let velStartPos = createVector(100,100);
-		let velMult = 20;
-		line(velStartPos.x, velStartPos.y, velStartPos.x + this.vel.x * velMult,
-											 velStartPos.y + this.vel.y * velMult);
-		line(width - velStartPos.x, velStartPos.y, width - velStartPos.x + this.vel.x * velMult,
-											 velStartPos.y + this.vel.y * velMult);
+	draw() {
+		strokeWeight(1);
+		ellipse(this.pos.x, this.pos.y, this.r * 2);
 	}
 }
 
-class LineSegment{
-	constructor(x1,y1,x2,y2){
-		this.p1 = createVector(x1,y1);
-		this.p2 = createVector(x2,y2);
+class LineSegment {
+	constructor(x1, y1, x2, y2) {
+		this.start_offset = width - 50;
 
-		if (this.p1.x > this.p2.x){
+		this.p1 = createVector(x1 + this.start_offset, y1);
+		this.p2 = createVector(x2 + this.start_offset, y2);
+
+		if (this.p1.x > this.p2.x) {
 			[this.p1, this.p2] = [this.p2, this.p1];
 		}
+
+		this.move_away = false;
 	}
 
-	draw(){
+	draw() {
+		if (this.move_away) {
+			this.p1.x -= landSlideSpeed;
+			this.p2.x -= landSlideSpeed;
+		}
+		if (this.start_offset > 0) {
+			this.p1.x -= landSlideSpeed;
+			this.p2.x -= landSlideSpeed;
+
+			this.start_offset -= landSlideSpeed;
+		}
+
+		strokeWeight(3);
 		line(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
 	}
-
 }
 
-class Dot{
-	constructor(x,y){
+class Dot {
+	constructor(x, y) {
 		this.x = x;
 		this.y = y;
 	}
 }
 
-function mousePressed(){
+function initStartPoint(ball, lineSegments, dots) {
+	//    _o_
+	// __/   \__
+
+	let leftBorder = 0;
+	let rightBorder = dots[0].x;
+	let y = dots[0].y;
+
+	let gapSize = ball.r * 6;
+	let depthSize = ball.r * 1.2;
+	let groundSize = (rightBorder - leftBorder - gapSize) / 2;
+
+	lineSegments.push(new LineSegment(leftBorder, y, leftBorder + groundSize, y));
+	lineSegments.push(new LineSegment(leftBorder + groundSize, y, leftBorder + groundSize + gapSize / 10, y - depthSize));
+	lineSegments.push(new LineSegment(leftBorder + groundSize + gapSize / 10, y - depthSize, leftBorder + groundSize + gapSize - gapSize / 10, y - depthSize));
+	lineSegments.push(new LineSegment(leftBorder + groundSize + gapSize - gapSize / 10, y - depthSize, leftBorder + groundSize + gapSize, y));
+	lineSegments.push(new LineSegment(rightBorder - groundSize, y, rightBorder, y));
+}
+
+function initEndPoint(ball, lineSegments, dots) {
+	// __     __
+	//   \_o_/
+
+	let leftBorder = dots[dots.length - 1].x;
+	let rightBorder = width;
+	let y = dots[dots.length - 1].y;
+
+	let gapSize = ball.r * 4;
+	let depthSize = ball.r * 4;
+	let groundSize = (rightBorder - leftBorder - gapSize) / 2;
+
+	lineSegments.push(new LineSegment(leftBorder, y, leftBorder + groundSize, y));
+	lineSegments.push(new LineSegment(leftBorder + groundSize, y, leftBorder + groundSize + gapSize / 10, y + depthSize));
+	lineSegments.push(new LineSegment(leftBorder + groundSize + gapSize / 10, y + depthSize, leftBorder + groundSize + gapSize - gapSize / 10, y + depthSize));
+	lineSegments.push(new LineSegment(leftBorder + groundSize + gapSize - gapSize / 10, y + depthSize, leftBorder + groundSize + gapSize, y));
+	lineSegments.push(new LineSegment(rightBorder - groundSize, y, rightBorder, y));
+
+	finishMark = createVector(leftBorder + groundSize + gapSize / 2, y + depthSize - ball.r / 2);
+}
+
+function mousePressed() {
 	if (keyIsDown(CONTROL)) {
+		ball.pos.x = mouseX;
+		ball.pos.y = mouseY;
+		ball.vel.x = 0;
+		ball.vel.y = 0;
+	} else if (mouseButton === LEFT) {
 		let force = createVector(mouseX - ball.pos.x, mouseY - ball.pos.y);
-		force.setMag(force.mag()/100);
+		force.setMag(force.mag() / 80);
 
 		if (ball.vel.mag() < 1) {
 			ball.vel.add(force);
 		}
-	} else if (mouseButton === LEFT) {
-		ball.pos.x = mouseX;
-		ball.pos.y = mouseY;
-		ball.vel.x = 0; ball.vel.y = 0;
-	} 
+	}
+}
+
+function keyPressed() {
+	if (keyCode === ENTER) {
+		initLevel();
+	}
 }
