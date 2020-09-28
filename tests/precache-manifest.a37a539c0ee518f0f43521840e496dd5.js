28a3d66f1012a5efdc80abf5f369307b29402d7c
vkulinenko-stud <shoco13@gmail.com>
2020-09-28 18:56:25 +0700

    tests

---
+++ tests/precache-manifest.a37a539c0ee518f0f43521840e496dd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f1dcd24252ee48596c85ecbf3efbef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed3bb431b4dad32a2fb4",
+    "url": "/static/css/main.3bbc922c.chunk.css"
+  },
+  {
+    "revision": "ad6af7385db14ce8c720",
+    "url": "/static/js/2.8aa02cca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8aa02cca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed3bb431b4dad32a2fb4",
+    "url": "/static/js/main.a99d6961.chunk.js"
+  },
+  {
+    "revision": "03d854ab401cfbfd5f54",
+    "url": "/static/js/runtime-main.bf563a2d.js"
+  }
+]);
