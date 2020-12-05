c3781413ad43d306afecc28ea1f6e4983b810b49
Victor Cook <shoco13@gmail.com>
2020-12-05 21:56:39 +0700

    5

--- tests/service-worker.js
+++ tests/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d44fe7724338a6a412357ad0081b3981.js"
+  "./precache-manifest.5cc9d54caad8752f0e2ce8b95dfefd8c.js"
 );
 
 self.addEventListener('message', (event) => {
