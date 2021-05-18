1861afa6d7c83b35adce8c4d351da38e6fc30b61
Victor Cook <shoco13@gmail.com>
2021-05-18 17:09:54 +0700

    app fix

--- simple_test_system/service-worker.js
+++ simple_test_system/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e06aecf2382e0869172d1cef7242a00d.js"
+  "./precache-manifest.dad2436347837a7e51df11ea8bf25428.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
